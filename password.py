e06e45e20b707e2ae258a3fcbf31aeed7b5b0c76
Hassan Gelle <hassangelle45@hotmail.com>
2021-04-28 10:25:51 +0200

    Made a minor adjustments to the first code inn password.py.

--- password.py
+++ password.py
@@ -6,7 +6,7 @@ from random import randint
 # All uppercase password
 password = ""
 for i in range(10):
-    i = chr(randint(65, 90))
+    i = chr(randint(65, 90)).lower()
     password = str(password) + i
 print(password)
 
--- password_generator.py
+++ password_generator.py
@@ -1,1 +1,27 @@
-# password generator 2 
+# password generator 2
+import random
+
+uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÆØ"
+lowercase_letters = uppercase_letters.lower()
+digits = "0123456789"
+symbols = "()[]{},;:._!@£$€&/=?``\\*+-"
+
+upper, lower, nums, syms = True, True, True, True
+
+all = ""
+
+if upper:
+    all += uppercase_letters
+if lower:
+    all += lowercase_letters
+if nums:
+    all += digits
+if syms:
+    all += symbols
+
+length = 15
+amount = 10
+
+for i in range(amount):
+    password = "".join(random.sample(all, length))
+    print(password)
